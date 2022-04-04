7f28283fcb8571e383e34e5a23618fd673fd73bc
youssefshawkat <68171635+youssefshawkat@users.noreply.github.com>
2022-04-04 22:18:00 +0200

    Java Router Simulation using Semaphore.

---
+++ semaphore.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class semaphore {
+    protected int value;
+    protected semaphore() { value = 0 ; }
+    protected semaphore(int initial) { value = initial ; }
+    public synchronized void P() {
+        value-- ;
+        if (value < 0)
+            try {
+                wait() ;
+            } catch( InterruptedException e ) { }
+
+    }
+    public synchronized void V() {
+
+
+        value++ ; if (value <= 0) notify() ;
+    }
+}
